2069c722c463e34ef7b3e11c6272b2361ec79fbd
Satya <swinargaa@gmail.com>
2023-05-29 22:18:43 +0200

    implemented time tracking and changed styling

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,22 @@ import { useState, useEffect } from 'react'
 import {nanoid} from "nanoid"
 import Confetti from "react-confetti"
 import Die from "./Die"
+import Stats from "./Stats"
 
 export default function App() {
+    // localStorage.removeItem("records")
     const [dice, setDice] = useState(allNewDice())
     const [tenzies, setTenzies] = useState(false)
     const [rollCount, setRollCount] = useState(0)
+    const [time, setTime] = useState(0)
+    const [isTrackingTime, setIsTrackingTime] = useState(false)
+    const [records, setRecords] = useState(JSON.parse(localStorage.getItem("records")) || [])
+    
+    // Seconds calculation
+    const seconds = Math.floor((time / 1000) % 60);
+
+    // // Milliseconds calculation
+    const milliseconds = (time / 10) % 100;
 
     useEffect(() => {
         const allHeld = dice.every(die => die.isHeld)
@@ -14,8 +25,39 @@ export default function App() {
         const allSameValue = dice.every(die => die.value === firstValue)
         if (allHeld && allSameValue) {
             setTenzies(true)
+            setIsTrackingTime(false)
         }
     }, [dice])
+
+    // track time
+    useEffect(() => {
+        let intervalId;
+        if (isTrackingTime) {
+            // setting time from 0 to 1 every 10 milisecond using javascript setInterval method
+            intervalId = setInterval(() => setTime(time => time + 10), 10);
+            }
+
+        return () => {
+            return clearInterval(intervalId)
+        };
+    }, [isTrackingTime]);
+
+    // store record to browser's local storage
+    useEffect(() => {
+        if (tenzies) {
+            setRecords(prevRecords => {
+                const newRecords = [{
+                    id: nanoid(),
+                    seconds,
+                    milliseconds,
+                    rollCount
+                },
+                ...prevRecords]
+                localStorage.setItem("records", JSON.stringify(newRecords))
+                return newRecords
+            })
+        }
+    }, [tenzies])
 
     function generateNewDie() {
         return {
@@ -34,6 +76,10 @@ export default function App() {
     }
 
     function rollDice() {
+        if (!isTrackingTime && rollCount == 0) {
+            setIsTrackingTime(prevVal => !prevVal)
+        }
+
         if(!tenzies) {
             setDice(oldDice => oldDice.map(die => {
                 return die.isHeld ? 
@@ -42,6 +88,8 @@ export default function App() {
             }))
             setRollCount(oldVal => oldVal + 1)
         } else {
+            setIsTrackingTime(false)
+            setTime(0)
             setTenzies(false)
             setDice(allNewDice())
             setRollCount(0)
@@ -49,11 +97,24 @@ export default function App() {
     }
 
     function holdDice(id) {
+        // the moment the first die gets clicked, the timer should start
+        if (dice.every(die => !die.isHeld)) setIsTrackingTime(true)
+
         setDice(oldDice => oldDice.map(die => {
             return die.id === id ? 
                 {...die, isHeld: !die.isHeld} :
                 die
         }))
+    }
+
+    function reset() {
+        if (rollCount > 0) {
+            setIsTrackingTime(false)
+            setTime(0)
+            setTenzies(false)
+            setDice(allNewDice())
+            setRollCount(0)
+        }
     }
 
     const diceElements = dice.map(die => (
@@ -63,26 +124,57 @@ export default function App() {
             isHeld={die.isHeld} 
             holdDice={() => holdDice(die.id)}
         />
+        
     ))
 
     return (
-    <main className="container">
-        {tenzies && <Confetti />}
-        <h1 className="title">Tenzies</h1>
-        <p className="instructions">Roll until all dice are the same. 
-        Click each die to freeze it at its current value between rolls.</p>
-        <div className="dice-container">
-            {diceElements}
+        <div className="container">
+            {tenzies && <Confetti width={window.innerwidth}/>}
+            <div className="row">
+                <main className="
+                        col-lg-8 
+                        container-fluid 
+                        d-flex 
+                        flex-column 
+                        justify-content-center 
+                        align-items-center"
+                >
+                    <h1 className="title">Tenzies</h1>
+                    <p className="instructions">Roll until all dice are the same. 
+                    Click each die to freeze it at its current value between rolls.</p>
+                    <div className="dice-container">
+                        {diceElements}
+                    </div>
+                    <button 
+                        className="roll-dice" 
+                        onClick={rollDice}
+                    >
+                        {tenzies ? "New Game" : "Roll"}
+                    </button>
+                    <div className="
+                            container 
+                            d-flex 
+                            flex-row 
+                            justify-content-evenly 
+                            align-items-center
+                            p-2"
+                    >
+                        <div className="timer-text">
+                            Time: {seconds.toString().padStart(2, "0")}:
+                            {milliseconds.toString().padStart(2, "0")}
+                        </div>
+                        <div className="roll-count">
+                            Rolls: {rollCount}
+                        </div>
+                    </div>
+                    <button className="reset-btn" onClick={reset}>
+                        Reset
+                    </button>
+                </main>
+                <div className="col-lg-4 mt-2 d-flex justify-content-center">
+                    <Stats records={records}/>
+                </div>
+            </div>
         </div>
-        <button 
-            className="roll-dice" 
-            onClick={rollDice}
-        >
-            {tenzies ? "New Game" : "Roll"}
-        </button>
-        <h4 className="roll-count">
-            Rolls: {rollCount}
-        </h4>
-    </main>
     )
 }
